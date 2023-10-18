cb14a551310b0afac4aa775a3ca692dcf97fc12e
azz <4zv4l@protonmail.com>
2023-10-18 16:28:20 +0800

    remove u128 and i128

--- types.h
+++ types.h
@@ -2,14 +2,12 @@ typedef unsigned char u8;
 typedef unsigned short int u16;
 typedef unsigned int u32;
 typedef unsigned long int u64;
-typedef unsigned long long int u128;
 typedef unsigned long long int usize;
 
 typedef char i8;
 typedef short int i16;
 typedef int i32;
 typedef long int i64;
-typedef long long int i128;
 typedef long long int isize;
 
 typedef float f32;
