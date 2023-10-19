08139ae1fbb37412a52ae73897b8ff567791b7b2
azz <4zv4l@protonmail.com>
2023-10-19 15:07:45 +0800

    add logging to regex.c

--- regex.c
+++ regex.c
@@ -8,13 +8,27 @@ match(char re[static 1], const char str[static 1], u32 *nmatch, u32 opt)
     char **groups = calloc(*nmatch, sizeof(char*));
 
     if (!matches)
+    {
+        log_warn("malloc(): couldnt alloc memory for matches");
         goto cleanup;
+    }
     if (!groups)
+    {
+
+        log_warn("malloc(): couldnt alloc memory for groups");
         goto cleanup;
+    }
     if (regcomp(&regex, re, opt))
+    {
+
+        log_warn("regcomp(): '%s' couldnt compile", re);
         goto cleanup;
+    }
     if (regexec(&regex, str, *nmatch, matches, 0))
+    {
+        log_warn("regexec(): '%s' doesnt match", str);
         goto all_cleanup;
+    }
 
     for (int match = 0; match < *nmatch; match++)
     {
@@ -22,7 +36,10 @@ match(char re[static 1], const char str[static 1], u32 *nmatch, u32 opt)
         {
             char **tmp = realloc(groups, sizeof(char*) * match);
             if (!tmp)
+            {
+                log_warn("realloc(): couldnt resize groups");
                 goto all_cleanup;
+            }
 
             *nmatch = match;
             groups = tmp;
@@ -32,7 +49,10 @@ match(char re[static 1], const char str[static 1], u32 *nmatch, u32 opt)
         int length = matches[match].rm_eo - matches[match].rm_so;
         groups[match] = malloc(length+1);
         if (!groups[match])
+        {
+            log_warn("malloc(): couldnt alloc memory for groups[%d]", match);
             goto all_cleanup;
+        }
 
         memcpy(groups[match], &str[matches[match].rm_so], length);
         groups[match][length] = 0;
@@ -45,7 +65,8 @@ match(char re[static 1], const char str[static 1], u32 *nmatch, u32 opt)
 all_cleanup:
     regfree(&regex);
 cleanup:
-    for (int i = 0; i < *nmatch && groups && groups[i]; i++) free(groups[i]);
+    for (int i = 0; i < *nmatch && groups && groups[i]; i++)
+        free(groups[i]);
     free(groups);
     free(matches);
     return 0;
