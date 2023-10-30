268bb9cfd1482dca8dc46f5cbe04eb0fc1b594ee
azz <4zv4l@protonmail.com>
2023-10-30 13:56:40 +0800

    md5 lib

---
+++ md5.c
@@ -0,0 +1,107 @@
+#include "md5.h"
+
+i32
+md5_digest_from_hex(const unsigned char digest_hex[static 32], unsigned char buff[static 16])
+{
+    for (int i = 0; i < 16; i++)
+    {
+        sscanf((char*)digest_hex, "%2hhx", &buff[i]);
+        digest_hex += 2;
+    }
+
+    return 1;
+}
+
+void
+md5_digest_hex(const unsigned char digest[static 16], unsigned char buff[static 32])
+{
+    for (int i = 0; i < 16; i++)
+        buff += sprintf((char *)buff, "%02x", digest[i]);
+}
+
+void
+md5_digest(const unsigned char digest[static 16])
+{
+    for (int i = 0; i < 16; i++)
+        printf("%02x", digest[i]);
+    printf("\n");
+}
+
+void
+md5_digest_ex(const unsigned char digest[static 16], const char str[static 1])
+{
+    for (int i = 0; i < 16; i++)
+        printf("%02x", digest[i]);
+    printf("  %s\n", str);
+}
+
+bool
+md5_expect(const char text[static 1], const unsigned char expected[static 1])
+{
+    unsigned char digest[MD5_DIGEST_LEN+1] = {0};
+
+    md5_from_str(text, digest);
+    if (memcmp(digest, expected, MD5_DIGEST_LEN) != 0)
+        return false;
+    return true;
+}
+
+bool
+md5_expect_hex(const char text[static 1], const unsigned char expected_hex[static 1])
+{
+    unsigned char digest[MD5_DIGEST_LEN+1]       = {0};
+    unsigned char expected[MD5_DIGEST_LEN+1]     = {0};
+
+    md5_digest_from_hex(expected_hex, expected);
+    md5_from_str(text, digest);
+
+    if (memcmp(digest, expected, MD5_DIGEST_LEN) != 0)
+        return false;
+    return true;
+}
+
+i32
+md5_from_file(const char filename[static 1], unsigned char digest[static 16])
+{
+    FILE *file = fopen(filename, "rb");
+    if (!file)
+        return fprintf(stderr, "%s: %s\n", filename, strerror(errno)), 0;
+
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    if (EVP_DigestInit_ex(ctx, EVP_md5(), NULL) == 0)
+        return fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), NULL)), 0;
+
+    char buff[1024] = {0};
+    int length = 0;
+    while((length = fread(buff, 1, 1024, file)) > 0)
+    {
+        if (EVP_DigestUpdate(ctx, buff, length) == 0)
+            return fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), NULL)), 0;
+    }
+
+    if (EVP_DigestFinal_ex(ctx, digest, NULL) == 0)
+        return fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), NULL)), 0;
+
+    fclose(file);
+    EVP_MD_CTX_free(ctx);
+
+    return 1;
+}
+
+i32
+md5_from_str(const char str[static 1], unsigned char digest[static 16])
+{
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    if (EVP_DigestInit_ex(ctx, EVP_md5(), NULL) == 0)
+        return fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), NULL)), 0;
+
+    if (EVP_DigestUpdate(ctx, str, strlen(str)) == 0)
+        return fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), NULL)), 0;
+
+    if (EVP_DigestFinal_ex(ctx, digest, NULL) == 0)
+        return fprintf(stderr, "%s\n", ERR_error_string(ERR_get_error(), NULL)), 0;
+
+    EVP_MD_CTX_free(ctx);
+
+    return 1;
+}
---
+++ md5.h
@@ -0,0 +1,49 @@
+#ifndef MD5
+#define MD5
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
+#include "types.h"
+
+#define MD5_DIGEST_LEN 16
+
+/*
+ * convert digest from hex and store it in buff (must be at least 16 char long)
+ */
+i32 md5_digest_from_hex(const unsigned char digest[static 32], unsigned char buff[static 16]);
+/*
+ * convert digest to hex and store it in buff (must be at least 32 char long)
+ */
+void md5_digest_hex(const unsigned char digest[static 16], unsigned char buff[static 32]);
+/*
+ * show the md5 digest in hex
+ */
+void md5_digest(const unsigned char digest[static 16]);
+/*
+ * show the md5 digest in hex followed by the string
+ */
+void md5_digest_ex(const unsigned char digest[static 16], const char str[static 1]);
+/*
+ * return if digest for string match or not
+ */
+bool md5_expect(const char text[static 1], const unsigned char expected[static 1]);
+/*
+ * return if digest (in hex format) for string match or not
+ */
+bool md5_expect_hex(const char text[static 1], const unsigned char expected_hex[static 1]);
+/*
+ * compute md5 from given filename
+ * return 1 if ok
+ * print error to stderr and return error code otherwise
+ */
+i32 md5_from_file(const char filename[static 1], unsigned char[static 16]);
+/*
+ * compute md5 of string (MUST be 0 terminated)
+ * return 1 if ok
+ * print error to stderr and return error code otherwise
+ */
+i32 md5_from_str(const char str[static 1], unsigned char[static 16]);
+#endif
